1d99adf371d16eb23e8614bf24874c3e28d5ef7c
yeonarim <sdr04161@gmail.com>
2019-06-29 21:10:35 +0900

    add login activity

---
+++ app/src/main/java/com/mashup/allnight/DrinkKindActivity.kt
@@ -0,0 +1,12 @@
+package com.mashup.allnight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DrinkKindActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drink_kind)
+    }
+}
---
+++ app/src/main/java/com/mashup/allnight/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.mashup.allnight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        
+    }
+}

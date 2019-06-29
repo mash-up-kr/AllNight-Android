891343a16bb4a876e72e9bd9f64fcab0a772d622
Aaron Roh <roharon98@gmail.com>
2019-06-29 22:12:04 +0900

    feat: create some recyclerView stuffs

---
+++ app/src/main/java/com/mashup/allnight/recipeMultipleActivity.kt
@@ -0,0 +1,13 @@
+package com.mashup.allnight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class recipeMultipleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mix_recipe_multiple)
+
+    }
+}
---
+++ app/src/main/java/com/mashup/allnight/recipeSingleActivity.kt
@@ -0,0 +1,29 @@
+package com.mashup.allnight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.mix_recipe_single.*
+
+class recipeSingleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mix_recipe_single)
+
+        init()
+    }
+
+    fun init() {
+        val list = ArrayList<MainListItem>()
+        list.add(MainListItem(0, "x", "test1"))
+        list.add(MainListItem(0, "x", "test2"))
+        list.add(MainListItem(0, "x", "test3"))
+        list.add(MainListItem(0, "x", "test4"))
+        list.add(MainListItem(0, "x", "test5"))
+        list.add(MainListItem(0, "x", "test6"))
+        val adapter = RecipeListAdapter(list)
+        cardView.adapter = adapter
+        cardView.layoutManager = LinearLayoutManager(this)
+    }
+}

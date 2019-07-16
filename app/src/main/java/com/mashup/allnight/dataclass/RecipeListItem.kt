a75f438c85676a2598ebafe301f1da59e272358c
yeonarim <sdr04161@gmail.com>
2019-07-17 03:39:37 +0900

    RecipeListItem수정

--- app/src/main/java/com/mashup/allnight/dataclass/RecipeListItem.kt
+++ app/src/main/java/com/mashup/allnight/dataclass/RecipeListItem.kt
@@ -1,10 +1,8 @@
 package com.mashup.allnight.dataclass
 
-import java.io.Serializable
-
 class RecipeListItem(
     val imageUrl: String,
     val title: String,
     var scraped: Boolean = false,
     val id: String
-):Serializable
+)

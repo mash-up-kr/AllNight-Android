d0e21f95e237468691d680474c51f8d0caefc9ae
roharon <roharon@studentpartner.com>
2019-07-19 13:36:38 +0900

    feat: 유저 최초접속 판단
    SharedPreference통한 유저 최초접속 판단

--- app/src/main/java/com/mashup/allnight/MySharedPreferences.kt
+++ app/src/main/java/com/mashup/allnight/MySharedPreferences.kt
@@ -9,6 +9,7 @@ import com.mashup.allnight.dataclass.RecipeListItem
 class MySharedPreferences(context: Context) {
     val PREFS_FILENAME = "prefs"
     val PREF_KEY_MY_SCRAP = "scrap"
+    val PREF_KEY_LEAD_OFF = "leadOff"
     val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0)
     val editor: SharedPreferences.Editor = prefs.edit()
 
@@ -33,6 +34,17 @@ class MySharedPreferences(context: Context) {
         var gson = Gson()
         var json: String = gson.toJson(values)
         editor.putString(PREF_KEY_MY_SCRAP, json)
+        editor.commit()
+    }
+
+    fun getUserLeadOff(): Boolean{
+        // 사용자 어플리케이션 최초 실행 - user leadoff
+        // return : 최초실행 : true, 이전접속O : false
+        var leadOff: Boolean = prefs.getBoolean(PREF_KEY_LEAD_OFF, true)
+    }
+
+    fun setUserLeadOff(value: Boolean){
+        editor.putBoolean(PREF_KEY_LEAD_OFF, value)
         editor.commit()
     }
 }

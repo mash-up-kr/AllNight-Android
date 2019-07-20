29bed32e47212e319fbec46ebd5550cb10d7d347
roharon <roharon@studentpartner.com>
2019-07-20 15:26:58 +0900

    modify: Login2Activity -> MainActivity  FLAG
    사용자 최초설정이후 돌아감을 방지하기 위한FLAG_ACTIVITY_NEW_TASK 추가

--- app/src/main/java/com/mashup/allnight/Login2Activity.kt
+++ app/src/main/java/com/mashup/allnight/Login2Activity.kt
@@ -18,6 +18,9 @@ class Login2Activity : AppCompatActivity() {
 
         no_button.setOnClickListener {
             val intent = Intent(this,MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            // 이전화면으로 돌아가지않도록 설정함
             startActivity(intent)
         }
     }

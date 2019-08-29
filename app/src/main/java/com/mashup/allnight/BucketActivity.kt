819eb284338bb6e8deb8b05a237e91042b7a47c3
yeonarim <sdr04161@gmail.com>
2019-08-30 04:48:20 +0900

    디자인 수정 영문 수정

--- app/src/main/java/com/mashup/allnight/BucketActivity.kt
+++ app/src/main/java/com/mashup/allnight/BucketActivity.kt
@@ -35,6 +35,18 @@ class BucketActivity : AppCompatActivity() {
             intent.putExtra(RecipeListActivity.RECIPE_LIST_TYPE_KEY, RecipeListActivity.RECIPE_LIST_TYPE_SEARCH)
             startActivity(intent)
         }
+
+        bucket_search_button.setOnClickListener{
+            val items: ArrayList<String> = arrayListOf()
+            for(i in 0 until adapter.getItemList().size) {
+                items.add(adapter.getItemList()[i].name)
+            }
+
+            val intent = Intent(this, RecipeListActivity::class.java)
+            intent.putExtra("item", items)
+            intent.putExtra(RecipeListActivity.RECIPE_LIST_TYPE_KEY, RecipeListActivity.RECIPE_LIST_TYPE_SEARCH)
+            startActivity(intent)
+        }
     }
     fun initItemData(): MutableList<DataList> {
 
